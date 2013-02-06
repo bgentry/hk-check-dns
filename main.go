e417e37d770a602ccc47a67e3d828209b5682a1f
Blake Gentry <blakesgentry@gmail.com>
2013-02-05 16:25:54 -0800

    HTTP Basic Auth

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
@@ -11,11 +12,12 @@ import (
 	"os"
 )
 
+var authuser, authpassword string
+
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "5000"
-	}
+	port := envStringOrDefault("PORT", "5000")
+	authuser = envStringOrDefault("AUTH_USER", "user")
+	authpassword = envStringOrDefault("AUTH_PASSWORD", "changeme")
 
 	r := mux.NewRouter()
 	r.NotFoundHandler = http.HandlerFunc(NotFoundHandler)
@@ -34,11 +36,58 @@ func main() {
 	}
 }
 
+func envStringOrDefault(name, defaultvalue string) string {
+	val := os.Getenv(name)
+	if val == "" {
+		return defaultvalue
+	}
+	return val
+}
+
+func getBasicAuth(r *http.Request) (username string, password string) {
+	authString := r.Header.Get("Authorization")
+	if len(authString) > 6 && string(authString[0:5]) == "Basic" {
+		creds, err := base64.StdEncoding.DecodeString(authString[6:])
+		if err != nil {
+			log.Printf("level=error type=credential_decode_error message=%q", err.Error())
+			return "", ""
+		}
+		username, password = split(string(creds), ':', true)
+	}
+	return
+}
+
+func split(s string, c byte, cutc bool) (string, string) {
+	for i := 0; i < len(s); i++ {
+		if s[i] == c {
+			if cutc {
+				return s[0:i], s[i+1:]
+			}
+			return s[0:i], s[i:]
+		}
+	}
+	return s, ""
+}
+
+func checkAuth(r *http.Request, username string, password string) bool {
+	u, p := getBasicAuth(r)
+	return u == username && p == password
+}
+
+func UnauthorizedResponse(w http.ResponseWriter) {
+	http.Error(w, "{\"error\": \"unauthorized\"}", 401)
+}
+
 func NotFoundHandler(w http.ResponseWriter, req *http.Request) {
 	http.Error(w, "{\"error\": \"not found\"}", 404)
 }
 
 func LookupHandler(w http.ResponseWriter, req *http.Request) {
+	if !checkAuth(req, authuser, authpassword) {
+		UnauthorizedResponse(w)
+		return
+	}
+
 	domainname := dns.Fqdn(mux.Vars(req)["hostname"])
 	nocache := req.URL.Query().Get("nocache") != ""
 
@@ -52,6 +101,11 @@ func LookupHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func VerifyTargetHandler(w http.ResponseWriter, req *http.Request) {
+	if !checkAuth(req, authuser, authpassword) {
+		UnauthorizedResponse(w)
+		return
+	}
+
 	hostname1 := dns.Fqdn(mux.Vars(req)["hostname1"])
 	hostname2 := dns.Fqdn(mux.Vars(req)["hostname2"])
 	nocache := req.URL.Query().Get("nocache") != ""
